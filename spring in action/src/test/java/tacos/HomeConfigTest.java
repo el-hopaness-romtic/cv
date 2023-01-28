63f8ee61e21437ec69ab82fed03d0572ded82d77
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-01-28 21:17:08 +0300

    remove controller for 'home'

--- spring in action/src/test/java/tacos/HomeConfigTest.java
+++ spring in action/src/test/java/tacos/HomeConfigTest.java
@@ -3,14 +3,15 @@ package tacos;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import tacos.web.WebConfig;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
-class HomeControllerTest {
+@WebMvcTest @ContextConfiguration(classes = WebConfig.class)  class HomeConfigTest {
     @Autowired
     MockMvc mockMvc;
 
