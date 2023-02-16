264a70dcf4bf25e8f58a9ae71767dbc2683670c8
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-02-16 14:18:17 +0300

    formatting

--- spring in action/src/main/java/tacos/data/IngredientRepository.java
+++ spring in action/src/main/java/tacos/data/IngredientRepository.java
@@ -3,6 +3,5 @@ package tacos.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tacos.Ingredient;
 
-public interface IngredientRepository
-        extends JpaRepository<Ingredient, String> {
+public interface IngredientRepository extends JpaRepository<Ingredient, String> {
 }
--- spring in action/src/main/java/tacos/data/OrderRepository.java
+++ spring in action/src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,5 @@ package tacos.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tacos.TacoOrder;
 
-public interface OrderRepository
-        extends JpaRepository<TacoOrder, Long> {
+public interface OrderRepository extends JpaRepository<TacoOrder, Long> {
 }
