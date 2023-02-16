2c6a93216777eb0d4d3ed40dc98fd11c0e0ad10f
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-02-16 17:26:25 +0300

    auth with InMemoryUserDetailsManager

---
+++ spring in action/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+import java.util.Arrays;
+
+import static java.util.Collections.singletonList;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(Arrays.asList(
+                new User(
+                        "buzz", encoder.encode("password"), singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                ),
+                new User(
+                        "woody", encoder.encode("password"), singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                )
+        ));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/design", "/orders").access(new WebExpressionAuthorizationManager("hasRole('USER')"))
+                        .anyRequest().access(new WebExpressionAuthorizationManager("permitAll()"))
+                )
+                .formLogin().loginPage("/login").defaultSuccessUrl("/design")
+                .and().build();
+    }
+}
