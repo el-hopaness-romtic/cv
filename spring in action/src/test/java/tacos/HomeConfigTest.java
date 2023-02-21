df869dee762234a3877a4d70e069c3783ee3fd9b
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-02-21 20:45:42 +0300

    add csrf token on /design page's form

--- spring in action/src/test/java/tacos/HomeConfigTest.java
+++ spring in action/src/test/java/tacos/HomeConfigTest.java
@@ -2,6 +2,7 @@ package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,7 +12,9 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest @ContextConfiguration(classes = WebConfig.class)  class HomeConfigTest {
+@WebMvcTest(excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@ContextConfiguration(classes = WebConfig.class)
+class HomeConfigTest {
     @Autowired
     MockMvc mockMvc;
 
