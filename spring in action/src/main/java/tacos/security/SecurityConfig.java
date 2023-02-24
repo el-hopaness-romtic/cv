3899a6b27d70df36675df67e6770756cd4738181
el-hopaness-romtic <60661917+el-hopaness-romtic@users.noreply.github.com>
2023-02-24 20:16:11 +0300

    toH2Console() instead of creating MvcRequestMatcher

--- spring in action/src/main/java/tacos/security/SecurityConfig.java
+++ spring in action/src/main/java/tacos/security/SecurityConfig.java
@@ -1,8 +1,8 @@
 package tacos.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,8 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import tacos.data.UserRepository;
 
 @Configuration
@@ -32,17 +30,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector handlerMappingIntrospector) throws Exception {
-        MvcRequestMatcher mvcRequestMatcher = new MvcRequestMatcher.Builder(handlerMappingIntrospector)
-                .servletPath("/h2-console")
-                .pattern(HttpMethod.POST, "/**");
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/design", "/orders").access(new WebExpressionAuthorizationManager("hasRole('USER')"))
                         .anyRequest().permitAll()
                 )
-                .csrf().ignoringRequestMatchers(mvcRequestMatcher).and()
+                .csrf().ignoringRequestMatchers(PathRequest.toH2Console()).and()
                 .headers().frameOptions().sameOrigin().and()
                 .formLogin().loginPage("/login").defaultSuccessUrl("/design").and()
                 .logout().logoutSuccessUrl("/").and()
